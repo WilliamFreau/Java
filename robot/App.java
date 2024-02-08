ee5661613d3b548aba1574331860cb3a0e575210
WilliamFREAU <38540996+WilliamFreau@users.noreply.github.com>
2024-02-08 08:27:24 +0100

    Create App.java

---
+++ robot/App.java
@@ -0,0 +1,13 @@
+package robot;
+
+/**
+ * Main Application file
+ */
+public class App {
+    
+    public static void main(String[] args) {
+        RobotEdison robotEdison = new RobotEdison();
+        Start.demarrer(robotEdison);
+    }
+    
+}
